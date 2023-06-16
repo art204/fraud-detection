8e9d7cb73a38c0634027f85c9010ddcb46b983cf
ArtemD2204 <35744124+ArtemD2204@users.noreply.github.com>
2023-06-16 19:56:50 +0300

    Update fastapi_service_test.ipynb

--- fastapi_service/settings.py
+++ fastapi_service/settings.py
@@ -1,4 +1,11 @@
 class Settings:
+    '''
+    Available models:
+    Fully connected neural network: 'fcnn'
+    Logistic regression: 'logreg'
+    Boosting: 'catboost', 'xgboost'
+    Stacking classifier with 3 estimators (CatBoost, XGB, LGBM): 'staking'
+    '''
 
     __model_name = 'fcnn'
 
